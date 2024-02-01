91361dd690dbdcff1c88c6a44ffc3470c56c0e5b
Scrapy Scrapygoofy Goofy <scrapygoofy@MacBook-Pro-de-Scrapy.local>
2024-02-01 17:00:19 -0500

    Taller Sesion2 y sesion3

---
+++ ejercicios.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+
+lista = [1, 9, 5, 0, 20, -4, 12, 16, 7]
+
+obj = 12
+
+for i in lista:
+    for j in lista:
+        if (i + j == obj):
+            print("Los numeros son:", i, ", ",j)
+            
+#%%
+
+l1 = [1, 9, 5, 0, 20, -4, 12, 16, 7]
+
+l2 = copy.l1
+print(l2)
+
+#copy.l1
+
